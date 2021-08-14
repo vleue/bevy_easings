9ec3373e95aeb4e8a6c48501b27667be9515375e
dinococo <61065398+undinococo@users.noreply.github.com>
2021-08-14 15:04:24 -0300

    Fixed compiler errors
    The compiler would complain about unstable features and float interpolation, so I fixed it.

--- bevy_easings/src/lib.rs
+++ bevy_easings/src/lib.rs
@@ -4,12 +4,11 @@
     trivial_casts,
     trivial_numeric_casts,
     unsafe_code,
-    unstable_features,
     unused_import_braces,
     unused_qualifications,
     missing_docs
 )]
-
+#![feature(float_interpolation)]
 //! Ease plugin for Bevy
 
 use std::time::Duration;
